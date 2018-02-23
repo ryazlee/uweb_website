f8f2929a31e164ea2bc9a563888e5a97eba689e3
ryazlee <ryan.j.lee99@gmail.com>
2018-02-23 09:06:26 -0800

    added the committeese page (which has a different format than the original).  working on the contact page and implementing a google map

--- js/myscript.js
+++ js/myscript.js
@@ -2,6 +2,18 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
